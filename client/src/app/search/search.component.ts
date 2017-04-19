af7c0bdb7d9f9f2583926fc4edac027a75616cc5
GLOBBIMUS <striker1342566@gmail.com>
2017-04-18 21:19:21 -0500

    Clicked flower on search page makes up url and navigates to it. #7

--- client/src/app/search/search.component.ts
+++ client/src/app/search/search.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit} from '@angular/core';
 import { SearchService } from "./search.service";
 import { FilterBy } from "./filter.pipe";
 import { Plant } from "../garden/plants/plant";
+import {Router} from "@angular/router";
+
 
 @Component({
     selector: 'search-component',
@@ -12,7 +14,13 @@ import { Plant } from "../garden/plants/plant";
 export class SearchComponent implements OnInit {
     public plants: Plant[];
 
-    constructor(private searchService: SearchService) {
+    constructor(private searchService: SearchService, private router: Router) {
+    }
+
+    public pickFlower(plant: Plant){
+        var bedID: string = plant.gardenLocation;
+        var plantID: string =  plant.id;
+        this.router.navigateByUrl("/" + bedID + "/" + plantID);
     }
 
 
