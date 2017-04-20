b39f22e5178b4d2db8c5e248c8b124944cc8a1c4
cookx876 <cookx876@morris.umn.edu>
2017-04-19 20:37:19 -0500

    Add Admin test (kinda)
    #2 and #3

---
+++ server/src/test/java/umm3601/admin/AdminTest.java
@@ -0,0 +1,25 @@
+package umm3601.admin;
+
+import org.junit.Before;
+import umm3601.digitalDisplayGarden.PlantController;
+import umm3601.admin.*;
+import umm3601.digitalDisplayGarden.Admin;
+
+import java.io.IOException;
+
+/**
+ * Created by cookx876 on 4/19/17.
+ */
+public class AdminTest {
+
+    private final static String databaseName = "data-for-testing-only";
+
+    @Before
+    public void makeDB() throws IOException {
+//        CreateDatabase
+
+    }
+
+
+
+}
