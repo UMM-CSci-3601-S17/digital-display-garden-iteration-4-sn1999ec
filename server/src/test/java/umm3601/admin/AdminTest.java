34d9d4502eb436b82307caf0c1df0e466506a088
cookx876 <cookx876@morris.umn.edu>
2017-04-21 14:03:45 -0500

    Fix Admin tests
    #2 and #3

--- server/src/test/java/umm3601/admin/AdminTest.java
+++ server/src/test/java/umm3601/admin/AdminTest.java
@@ -1,25 +1,33 @@
 package umm3601.admin;
 
 import org.junit.Before;
+import org.junit.Test;
 import umm3601.digitalDisplayGarden.PlantController;
 import umm3601.admin.*;
 import umm3601.digitalDisplayGarden.Admin;
 
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 
-/**
- * Created by cookx876 on 4/19/17.
- */
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
+
 public class AdminTest {
 
     private final static String databaseName = "data-for-testing-only";
+    private Admin admin;
 
     @Before
-    public void makeDB() throws IOException {
-//        CreateDatabase
-
+    public void makeDB() throws IOException, NoSuchAlgorithmException {
+       admin = new Admin();
+       admin.CreateDatabase();
     }
 
-
+    @Test
+    public void checkPassword() throws Exception {
+       assertFalse(admin.checkPassword("NotTheCorrectPassword"));
+       assertTrue(admin.checkPassword("passwordToBeHashed"));
+    }
 
 }
