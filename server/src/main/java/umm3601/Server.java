d89592cdb0cfbd70d575ac6a103c440307093f37
nathan-beneke <benek020@morris.umn.edu>
2017-04-21 17:28:58 -0500

    Change path and password to keystore.jks

--- server/src/main/java/umm3601/Server.java
+++ server/src/main/java/umm3601/Server.java
@@ -32,7 +32,7 @@ public class Server {
 
         //Note: This keystore is stolen from Spark's FAQ. We need to create our own at some point
         //and get it signed by a trusted authority.
-        secure("keystore.jks", "password", null, null);
+        secure("/keystore.jks", "sn1999ec", null, null);
 
         port(2538);
 
